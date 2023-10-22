c1e100ea1cf70a9b27501cb4c2337655990e4024
bilgehanparay <bilgehanparay@gmail.com>
2023-10-22 12:52:50 +0000

    first commit

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,7 +1,60 @@
-use std::str::FromStr;
-use std::env;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
-fn main() {
+/*
+This directive tells the serde crate to examine the 
+type when the code is compiled and automatically generate code
+to parse a value of this type from data in format of HTML form
+in POST request
+*/
+#[derive(Deserialize)]
+struct GcdParameters{
+    n: u64,
+    m: u64,
+}
+
+fn main(){
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
+
+    println!("Serving on http://localhost:2500...");
+    server
+        .bind("127.0.0.1:2500").expect("error binding server to address")
+        .run().expect("error running server");
+}
+
+fn get_index() -> HttpResponse{
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+                </form>
+            "#,
+        )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse{
+    if form.n == 0 || form.m == 0{
+        return HttpResponse::BadRequest()
+                .content_type("type/html")
+                .body("Computing the GCD with zero is boring.");
+    }
+    let response = format!("The gcd of numbers {} and {} is <b>{}</b>\n",
+                            form.n, form.m, gcd(form.n, form.m));
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
+/*
+fn main_gets_args() {
     let mut numbers = Vec::new();
 
     for arg in env::args().skip(1) {
@@ -19,7 +72,7 @@ fn main() {
     }
     println!("The greates common divisor of {:?} is {}", numbers, d)
 }
-
+*/
 fn gcd(mut n: u64, mut m: u64) -> u64{
     assert!(n != 0 && m!= 0);
     while m != 0 {
